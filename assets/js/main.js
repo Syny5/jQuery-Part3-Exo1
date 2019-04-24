23e078a0927095ad87fd63f8a6bcf5efd5acd724
syn <vicdemilly@gmail.com>
2019-04-24 15:20:50 +0200

    Ajout de commentaires

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
-var count = 0;
-$('#spamClick').click(function(){
-  count++;
-  $('#countClick').attr('value', count);
+$(function() {
+  // on déclare une variable count qu'on initialise à zéro
+  var count = 0;
+  $('#spamClick').click(function(){
+    // L'événement se produit au clic 
+    count++;
+    // on incrémente la variable
+    $('#countClick').attr('value', count);
+    // on donne au champs de texte la valeur de la variable
+  });
 });
